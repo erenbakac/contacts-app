df55f3089778c1e372c41f3e60980b60593ec806
erenbakac <erenbakac@gmail.com>
2023-06-14 21:29:24 +0300

    13

---
+++ src/components/Contacts/List/index.js
@@ -0,0 +1,20 @@
+import React, { useEffect } from 'react'
+
+function List({contacts}) {
+  
+  
+  return (
+    <div>
+      <ul>
+      {
+           contacts.map ((contact,index) =>  <li key={index}>{contact.fullname}</li>) }
+
+      </ul>
+    </div>
+
+
+  )
+}
+
+export default List
+
